ab2e63c887bda07081a677a2b0ed2f0419228756
Dreamyfox <farya@gmu.edu>
2025-10-25 20:59:34 -0400

    Update and rename Weighted_Grade_Calculator.py to Grade_Calculator.py

--- Personal_programming/Grade_Calculator.py
+++ Personal_programming/Grade_Calculator.py
@@ -1,41 +1,33 @@
-'''
-Checking to see how doomed am I 
-
-'''
-class grade:
+class Grade:
     def __init__(self):
-        self.name = ''
+        self.grade_component = ''
         self.percentage = ''
         self.sum_total = ''
         self.level = ''
         self.ask = ''
         self.num_components = ''
 
-
-
-
     def grade_calculator(self):
-        self.ask= input("Do you wish to proceed? (Y/N): ")
+        self.ask = input("Do you wish to proceed? (Y/N): ")
         while self.ask == 'Y' or self.ask == 'y':
             self.level = int(input("How many components is the grading divided into: "))
             self.sum_total = 0
             self.num_components = 0
-            while 0< self.level:
+            while 0 < self.level:
                 self.num_components += 1
-                self.name = int(input(f"Enter {self.num_components} grade number: "))
+                self.grade_component = int(input(f"Enter grade number {self.num_components}: "))
                 self.percentage = int(input("Enter percentage: "))
-                math = (self.name * self.percentage) / 100
+                math = (self.grade_component * self.percentage) / 100
                 self.sum_total += math
                 self.level -= 1
-            self.ask= input("Do you wish to proceed? (Y/N): ")
+            self.ask = input("Do you wish to proceed? (Y/N): ")
 
     def __str__(self):
-        return f'your final grade is: { str(self.sum_total)}'
-
+        return f'Your final grade is: {str(self.sum_total)}'
 
 
 def main():
-    grade1 = grade()
+    grade1 = Grade()
     grade1.grade_calculator()
     print(grade1)
 
